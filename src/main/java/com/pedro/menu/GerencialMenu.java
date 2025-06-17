cc5dbcdc8b78679a20c8df4fa49c4d7c3dc58b86
Pedro Cunha <idk.pedroc@gmail.com>
2025-06-17 16:04:40 -0300

    add: testes in IO

--- src/main/java/com/pedro/menu/GerencialMenu.java
+++ src/main/java/com/pedro/menu/GerencialMenu.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 import com.pedro.config.IO;
 import com.pedro.models.Aluno;
 import com.pedro.service.AlunoService;
+import com.pedro.service.LivroService;
+import com.pedro.service.ProfessorService;
 
 
 public class GerencialMenu {
@@ -16,7 +18,9 @@ public class GerencialMenu {
     private AutorMenu autorMenu;
     private GeneroMenu generoMenu;
     private EnderecoMenu enderecoMenu;
-    private AlunoService alunoService;
+    private AlunoService alunoService = new AlunoService();
+    private LivroService livroService = new LivroService();
+    private ProfessorService professorService = new ProfessorService();
 
     public GerencialMenu(){
         io = new IO();
@@ -27,10 +31,21 @@ public class GerencialMenu {
     }
 
     public void imprimirMenuGerencial(){
-        List<String> opcoesGerencial = new ArrayList<String>(Arrays.asList("1. Editora", "2. Autor", "3. Gênero", "4. Endereço", "5. Consultar Aluno por Locação","6. Voltar"));
+        List<String> opcoesGerencial = new ArrayList<String>(
+                Arrays.asList(
+                        "1. Editora",
+                        "2. Autor",
+                        "3. Gênero",
+                        "4. Endereço",
+                        "5. Consultar Aluno por Locação",
+                        "6. Consultar Locações por Livro",
+                        "7. Consultar Professor com mais Locações",
+                        "8. Voltar"
+                )
+        );
         int opc = io.imprimirMenuRetornandoOpcao(opcoesGerencial, "MENU GERENCIAL");
 
-        while (opc != 6) {
+        while (opc != 8) {
             switch (opc) {
                 case 1:
                     editoraMenu.imprimirMenu();
@@ -47,12 +62,33 @@ public class GerencialMenu {
                 case 5:
                     consultarAlunoPorLocacao();
                     break;
+                case 6:
+                    obterQLocPLivroID();
+                    break;
+                case 7:
+                    obterProfessorMaisLocou();
+                    break;
                 default:
                     System.out.println("Opção inválida.");
             }
             opc = io.imprimirMenuRetornandoOpcao(opcoesGerencial, "MENU GERENCIAL");
         }
 
+    }
+
+    private void obterProfessorMaisLocou() {
+        String resultado = professorService.obterProfessorComMaisLocacoes();
+        System.out.println(resultado);
+    }
+    private void obterQLocPLivroID() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o ID do livro: ");
+        String locacaoIDString = scanner.nextLine().trim();
+        int livroID = Integer.parseInt(locacaoIDString);
+        int qtd = livroService.obterQuantidadeDeLocacoesPorLivro(livroID);
+        if (qtd != 0) {
+            System.out.println("Quantidade de locações: " + qtd);
+        }
     }
 
     private void consultarAlunoPorLocacao() {
@@ -65,16 +101,16 @@ public class GerencialMenu {
             System.out.println("Locação não identificada.");
         } else {
             System.out.println("Aluno: ");
-            System.out.println(aluno.getId());
-            System.out.println(aluno.getNome());
-            System.out.println(aluno.getCpf());
-            System.out.println(aluno.getCurso());
-            System.out.println(aluno.getEmail());
+            System.out.println("Nome: " + aluno.getNome());
+            System.out.println("CPF: " + aluno.getCpf());
+            System.out.println("Curso: " + aluno.getCurso());
+            System.out.println("Email: " + aluno.getEmail());
         }
     }
 
     public static void main(String[] args) {
         GerencialMenu gerencial = new GerencialMenu();
+        System.out.println("RESULTADO: ");
         gerencial.imprimirMenuGerencial();
     }
 }
